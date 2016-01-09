0a746d6ec287064b354fd1815eb6bda21e6be3b7
Jimmy Tang <wnftng@gmail.com>
2016-01-10 07:55:15 +0800

    more signature tests

--- amazon_test.go
+++ amazon_test.go
@@ -41,7 +41,7 @@ func TestUnsignedAmazonRequest(t *testing.T) {
 		t.Errorf("\nEXPECTED:\n%v\nRESULT:\n%v", EXPECTED, RESULT)
 	} else { fmt.Println("TestUnsignedAmazonRequest: OK") }
 	
-}
+	}
 
 
 
@@ -72,13 +72,30 @@ AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&AssociateTag=mytag-20&ItemId=0679722769&Oper
 
 
 
-func TestSignedAmazonRequest(t *testing.T) {
+func TestSignedAmazonRequest_ItemLookup(t *testing.T) {
 	EXPECTED := "http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&AssociateTag=mytag-20&ItemId=0679722769&Operation=ItemLookup&ResponseGroup=Images%2CItemAttributes%2COffers%2CReviews&Service=AWSECommerceService&Timestamp=2014-08-18T12%3A00%3A00Z&Version=2013-08-01&Signature=j7bZM0LXZ9eXeZruTqWm2DIvDYVUU3wxPPpp%2BiXxzQc%3D"
 	
 	if RESULT := r.SignedURL(); RESULT != EXPECTED {
 		t.Errorf("\nEXPECTED:\n%v\nRESULT:\n%v", EXPECTED, RESULT)
-	} else { fmt.Println("TestSignedAmazonRequest: OK") }
+	} else { fmt.Println("TestSignedAmazonRequest_ItemLookup: OK") }
 }
 
+func TestSignedAmazonRequest_ItemSearch(t *testing.T) {
+	EXPECTED := "Gv4kWyAAD3xgSGI86I4qZ1zIjAhZYs2H7CRTpeHLD1o%3D"
+	
+	cStr := "GET\nwebservices.amazon.co.uk\n/onca/xml\nAWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Actor=Johnny%20Depp&AssociateTag=mytag-20&Operation=ItemSearch&Operation=ItemSearch&ResponseGroup=ItemAttributes%2COffers%2CImages%2CReviews%2CVariations&SearchIndex=DVD&Service=AWSECommerceService&Sort=salesrank&Timestamp=2014-08-18T17%3A34%3A34.000Z&Version=2013-08-01"
+	
+	if RESULT := amazon.HashSignature(cStr, r.Credentials.SecretKey); RESULT != EXPECTED {
+		t.Errorf("\nEXPECTED:\n%v\nRESULT:\n%v", EXPECTED, RESULT)
+	} else { fmt.Println("TestSignedAmazonRequest_ItemSearch: OK") }
+}
 
-
+func TestSignedAmazonRequest_CartCreate(t *testing.T) {
+	EXPECTED := "LpEUnc9tT4WGneeUwH0LvwxLLfbMEXgmjGX5GXQ1MEQ%3D"
+	
+	cStr := "GET\nwebservices.amazon.com\n/onca/xml\nAWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&AssociateTag=mytag-20&Item.1.OfferListingId=j8ejq9wxDfSYWf2OCp6XQGDsVrWhl08GSQ9m5j%2Be8MS449BN1XGUC3DfU5Zw4nt%2FFBt87cspLow1QXzfvZpvzg%3D%3D&Item.1.Quantity=3&Operation=CartCreate&Operation=ItemSearch&Service=AWSECommerceService&Timestamp=2014-08-18T17%3A36%3A55.000Z&Version=2013-08-01"
+	
+	if RESULT := amazon.HashSignature(cStr, r.Credentials.SecretKey); RESULT != EXPECTED {
+		t.Errorf("\nEXPECTED:\n%v\nRESULT:\n%v", EXPECTED, RESULT)
+	} else { fmt.Println("TestSignedAmazonRequest_CartCreate: OK") }
+}
